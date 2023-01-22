dfc301655632cbe4c2965c09ab11a1cee5af3827
Fabela <miguel.fabela@outlook.com>
2023-01-21 23:24:23 -0500

    Initial Commit

---
+++ MyAppRecipes/MyAppRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppRecipesApp.swift
+//  MyAppRecipes
+//
+//  Created by Consultant on 1/21/23.
+//
+
+import SwiftUI
+
+@main
+struct MyAppRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
